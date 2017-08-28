de583690451976aa245ad1a5eeb92f4d7bd94ad5
Jared Young <jyou338@gmail.com>
2017-08-28 19:48:05 +1200

    Lab Test 4.
    Made new prefab of sphere and assigned it to sketch and made every second instance a sphere in the for loop

--- Hello Unity/Assets/Sketch.cs
+++ Hello Unity/Assets/Sketch.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 
 public class Sketch : MonoBehaviour {
 
-    public GameObject myPrefab;
+    public GameObject myPrefab1;
+    public GameObject myPrefab2;
     public Material myMaterial;
 
 	void Start () {
-        int totalCubes = 30;
+        int totalPrefabs = 30;
         float totalDistance = 2.9f;
 
-        // Sin 
-        for (int i = 0; i < totalCubes; i++)
+        for (int i = 0; i < totalPrefabs; i++)
         {
-            float perc = i / (float)totalCubes;
+            float perc = i / (float)totalPrefabs;
 
-            float sin = Mathf.Sin(perc * Mathf.PI/2);
+            float sin = Mathf.Sin(perc * Mathf.PI / 2);
 
             float x = 1.8f + sin * totalDistance;
             float y = 5.0f;
             float z = 0.0f;
 
-            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-            newCube.GetComponent<CubeScript>().SetTime(i);
-            newCube.GetComponent<CubeScript>().SetSize(0.45f * (1.0f - perc));
-            newCube.GetComponent<CubeScript>().rotateSpeed = 0.2f + perc*4.0f;
-            newCube.GetComponent<CubeScript>().SetMaterial(myMaterial);
+            if (i % 2 == 0) { 
+                var newCube = (GameObject)Instantiate(myPrefab1, new Vector3(x, y, z), Quaternion.identity);
+                //newCube.GetComponent<CubeScript>().SetTime(i);
+                newCube.GetComponent<CubeScript>().SetSize(0.45f * (1.0f - perc));
+                newCube.GetComponent<CubeScript>().rotateSpeed = 0.2f + perc*4.0f;
+               //newCube.GetComponent<CubeScript>().SetMaterial(myMaterial);
+            } else
+            {
+                var newCube = (GameObject)Instantiate(myPrefab2, new Vector3(x, y, z), Quaternion.identity);
+                //newCube.GetComponent<CubeScript>().SetTime(i);
+                newCube.GetComponent<CubeScript>().SetSize(0.45f * (1.0f - perc));
+                newCube.GetComponent<CubeScript>().rotateSpeed = 0.2f + perc * 4.0f;
+                //newCube.GetComponent<CubeScript>().SetMaterial(myMaterial);
+            }
         }
 
     }
